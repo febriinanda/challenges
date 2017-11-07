cf892c190b216efa27c0616f3c340262faabfe68
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-11-08 02:53:16 +0700

    Pattern 3 done

---
+++ src/com/nanda/apps/Main.java
@@ -0,0 +1,11 @@
+package com.nanda.apps;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        NandaCls nandaObj = new NandaCls(5);
+        nandaObj.pattern3();
+        System.out.println(nandaObj.getPattern());
+    }
+}
