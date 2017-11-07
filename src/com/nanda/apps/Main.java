a4d355846ca785068f9cd4f4c3e20b176f0a65cc
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-11-08 03:50:38 +0700

    test push dev dari local

--- src/com/nanda/apps/Main.java
+++ src/com/nanda/apps/Main.java
@@ -3,7 +3,7 @@ package com.nanda.apps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here test commit
         NandaCls nandaObj = new NandaCls(5);
         nandaObj.pattern3();
         System.out.println(nandaObj.getPattern());
