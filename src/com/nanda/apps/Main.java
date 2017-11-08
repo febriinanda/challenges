0073a989c074e903345ccf29ae5162986912e6d5
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2017-11-08 07:24:59 +0700

    Hapus class yang tidak digunakan

--- src/com/nanda/apps/Main.java
+++ src/com/nanda/apps/Main.java
@@ -3,7 +3,7 @@ package com.nanda.apps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here test commit
+	// write your code here
 
         NumberPattern patternObj = new NumberPattern(5);
         patternObj.pattern10();
